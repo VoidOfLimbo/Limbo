065597afa87c54ef4c2a29c528bef3ba11376b83
VoidOfLimbo <bipin.paneru.9@gmail.com>
2026-04-13 23:10:32 +0100

    feat: update AppLogo component to hide text in collapsible state and change AuthLayout import to use AuthSplitLayout

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -6,7 +6,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
     <div class="flex items-center justify-center rounded-md dark:bg-white dark:p-1">
         <AppLogoIcon class="size-8" />
     </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
+    <div class="ml-1 grid flex-1 text-left text-sm group-data-[collapsible=icon]:hidden">
         <span class="mb-0.5 truncate leading-tight font-semibold"
             >VoidOfLimbo</span
         >
